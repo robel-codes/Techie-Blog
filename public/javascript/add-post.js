082f7ac56b2cab99f216616bba0fb17eedc04d9a
Robel <rofikre@yahoo.com>
2022-03-13 14:30:27 -0400

    added front logic to add post to dashboard

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,26 +1,28 @@
 async function newFormHandler(event) {
     event.preventDefault();
   
-    const post_title = document.querySelector('input[name="new-title"]').value;
-    const post_text = document.querySelector('input[name="new-text"]').value;
-  
-    const response = await fetch(`/api/posts`, {
-      method: 'POST',
-      body: JSON.stringify({
-        post_title,
-        post_text
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert(response.statusText);
+    const post_title = document.querySelector('input[name="new-title"]').value.trim();
+    const post_text = document.querySelector('textarea[name="new-text"]').value.trim();
+    
+    if(post_title && post_text){
+      const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({
+          post_title,
+          post_text
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+    
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
     }
   }
+}
   
-  document.querySelector('.create-post').addEventListener('click', newFormHandler);
+  document.querySelector('.form-new-post').addEventListener('submit', newFormHandler);
   
