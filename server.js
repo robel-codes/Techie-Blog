e17ff01ae78ecaa4089e94d355573f0cd1136112
Robel <rofikre@yahoo.com>
2022-03-13 01:09:54 -0500

    setup template engine and added helper fuctions

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 const express = require('express');
 const path = require('path')
-const routes = require('./controllers');
 const app = express();
-const session = require('express-session');
+//const session = require('express-session');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+//const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
 
-
+const PORT = process.env.PORT || 3001;
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const routes = require('./controllers');
 app.use(routes)
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(process.env.PORT, () => {
+    app.listen(PORT, () => {
         console.log('Now listening on 3001')
     })
 })
