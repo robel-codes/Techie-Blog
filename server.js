9a2676f15b51b201352ea930f51d1a5251c928fe
Robel <rofikre@yahoo.com>
2022-03-13 11:49:00 -0400

    created session on the backend

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const path = require('path')
 const app = express();
-//const session = require('express-session');
+const session = require('express-session');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-//const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
 
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: process.env.SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
