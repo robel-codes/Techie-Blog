85f507e25ff200e6b5533f3695fe3269efdd2db9
Robel <rofikre@yahoo.com>
2022-03-14 14:09:42 -0400

    modified index.js file to solve heroku issue

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,7 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api/');
-const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
-
-router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
-router.use('/api', apiRoutes);
+router.use('/', require('./home-routes.js'));
+router.use('/dashboard', require('./dashboard-routes.js'));
+router.use('/api', require('./api/'));
 
 module.exports = router;
